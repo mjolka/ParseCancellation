2c24717eb800ae0bcd8e613907140d9340d1b11e
Matt Collins <matt@mjolka.com>
2015-11-17 13:36:43 +1100

    Initial commit

---
+++ ParseCancellation/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using ParseCancellation.Services;
+
+namespace ParseCancellation
+{
+    public partial class MainWindow : Window
+    {
+        private readonly ParserService _parser = new ParserService();
+
+        // For debugging.
+        private int _parseRequestId;
+
+        private CancellationTokenSource _cancellationTokenSource;
+
+        private Task _parseTask;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Messages.ItemsSource = ParserMessages;
+        }
+
+        private ObservableCollection<ParseMessage> ParserMessages { get; } = new ObservableCollection<ParseMessage>();
+
+        private async void Parse(object sender, RoutedEventArgs e)
+        {
+            _cancellationTokenSource?.Cancel();
+            _cancellationTokenSource = new CancellationTokenSource();
+            _parseTask = ParseTaskHelper(_cancellationTokenSource.Token);
+            try
+            {
+                await _parseTask;
+            }
+            catch (OperationCanceledException)
+            {
+            }
+        }
+
+        private async Task ParseTaskHelper(CancellationToken cancellationToken)
+        {
+            try
+            {
+                if (_parseTask != null)
+                {
+                    await _parseTask;
+                }
+            }
+            catch (OperationCanceledException)
+            {
+            }
+
+            var parseRequestId = Interlocked.Increment(ref _parseRequestId);
+
+            cancellationToken.ThrowIfCancellationRequested();
+
+            // The Progress<T> constructor captures the current synchronization context,
+            // so we have to declare `progress` outside of the action passed to the
+            // Task constructor.
+            IProgress<string> progress = new Progress<string>(message =>
+            {
+                var item = new ParseMessage(parseRequestId, DateTime.Now, message);
+                ParserMessages.Add(item);
+                Messages.ScrollIntoView(item);
+            });
+
+            try
+            {
+                // Parse is a long-running, blocking, CPU-bound operation.
+                // Offload the work to the thread pool to keep the UI responsive.
+                await Task.Run(() => _parser.Parse(new[] {"abc", "xy"}, _cancellationTokenSource.Token, progress), _cancellationTokenSource.Token);
+            }
+            catch (OperationCanceledException)
+            {
+                progress.Report("\u274C Canceled");
+            }
+        }
+
+        private class ParseMessage
+        {
+            public ParseMessage(int parseRequestId, DateTime timestamp, string message)
+            {
+                ParseRequestId = parseRequestId;
+                Timestamp = timestamp.ToString("T");
+                Message = message;
+            }
+
+            public int ParseRequestId { get; }
+
+            public string Timestamp { get; }
+
+            public string Message { get; }
+        }
+
+        private async void ParseALot(object sender, RoutedEventArgs e)
+        {
+            // Simulate a bunch of button clicks to see if any exceptions
+            // are uncaught.
+            var synchronizationContext = SynchronizationContext.Current;
+            await Task.Run(() =>
+            {
+                for (var i = 0; i < 100; i++)
+                {
+                    synchronizationContext.Post(_ => { Parse(null, null); }, null);
+                }
+            });
+        }
+    }
+}
---
+++ ParseCancellation/Services/ParserService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParseCancellation.Services
+{
+    internal class ParserService
+    {
+        public void Parse(IEnumerable<string> values, CancellationToken cancellationToken, IProgress<string> progress = null)
+        {
+            progress = progress ?? new Progress<string>(_ => { });
+
+            var parallelOptions = new ParallelOptions { CancellationToken = cancellationToken };
+            Parallel.ForEach(values, parallelOptions, value =>
+            {
+                var parseResult = ParseInternal(value, progress, cancellationToken);
+                cancellationToken.ThrowIfCancellationRequested();
+                ResolveReferences(parseResult, progress, cancellationToken);
+            });
+
+            progress.Report("Done");
+        }
+
+        private static ParseResult ParseInternal(string value, IProgress<string> progress, CancellationToken cancellationToken)
+        {
+            progress.Report($"Parsing {value}...");
+
+            SimulateWork(cancellationToken);
+
+            cancellationToken.ThrowIfCancellationRequested();
+
+            progress.Report($"\u2714 Parsed {value}");
+
+            return new ParseResult(value);
+        }
+
+        private static void ResolveReferences(ParseResult parseResult, IProgress<string> progress, CancellationToken cancellationToken)
+        {
+            var parallelOptions = new ParallelOptions { CancellationToken = cancellationToken };
+            Parallel.ForEach(parseResult.Tree, parallelOptions, (c, _, index) =>
+            {
+                progress.Report($"Resolving refererence \"{c}\" of {parseResult.Tree}...");
+
+                SimulateWork(cancellationToken, TimeSpan.FromSeconds(index + 1));
+                cancellationToken.ThrowIfCancellationRequested();
+
+                progress.Report($"\u2714 Resolved refererence \"{c}\" of {parseResult.Tree}");
+            });
+        }
+
+        private static void SimulateWork(CancellationToken cancellationToken, TimeSpan? duration = null)
+        {
+            cancellationToken.WaitHandle.WaitOne(duration.GetValueOrDefault(TimeSpan.FromSeconds(1)));
+        }
+
+        private class ParseResult
+        {
+            public ParseResult(string tree)
+            {
+                Tree = tree;
+            }
+
+            public string Tree { get; }
+        }
+    }
+}
